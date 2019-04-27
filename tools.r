b626c848bbf3dd11e7cb5383c3e19a5dc2ecb32e
tarko <vladtarko@gmail.com>
2019-04-27 00:24:02 -0400

    Update tools.r

--- tools.r
+++ tools.r
@@ -8,50 +8,6 @@ library(data.table)
 library(readxl)
 library(dplyr)
 
-# creates a summary dataframe that can be used in RStudio --------------------- 
-# similar to the variable explorer in Stata, 
-# but which also includes the summary statistics.
-#     * This is useful particularly if you have a large dataset 
-#       with a very large number of variables with hard to remember names.
-#     * Can also be used to generate a table of summary statistics.
-#     * if `viewer` is TRUE (default) the result is shown in RStudio's Viewer pane
-#       as a searchable datatable
-var.explorer <- function(df, viewer = TRUE)
-{
-  
-  library(dplyr)
-  
-  # build summary
-  summary_df <- data.frame(
-      Variable    = names(df), 
-      Description = sjlabelled::get_label(df), 
-      t(pastecs::stat.desc(df)) 
-    ) %>% 
-    dplyr::select(
-      Variable, Description, 
-      Obs. = nbr.val, Missing = nbr.na, 
-      min, max, median, mean, std.dev)
-  
-  # use numbers as rownames, corresponding to the column numbers
-  rownames(summary_df) <- seq_along(summary_df$Variable)
-  
-  # if viewer = TRUE show as searchable datatable in the viewer pane
-  if(viewer) {
-    
-    tempFileName <- tempfile("summary_df_", fileext = ".html")
-    
-    summary_df %>% 
-      DT::datatable() %>% 
-      DT::formatRound(columns = 5:9, digits = 2) %>% 
-      DT::saveWidget(tempFileName)
-    
-    rstudioapi::viewer(tempFileName)
-  }
-  
-  return(summary_df)
-}
-
-
 # growth rate -----------------------------------------------------------------------
 
 growth.rate <- function(x, lag = 1) {
@@ -98,23 +54,7 @@ get.legend <- function(a.gplot)
   return(legend)
 }
 
-# reads a csv file with variable names on the first row -------------------------------
-# and the variable labels on the second row
-read_csv_with_labels <- function(fileName)
-{
-  # read the first two lines
-  varNames  <- read.csv(fileName, nrows = 1, stringsAsFactors = FALSE, header = FALSE)
-  varLabels <- read.csv(fileName, nrows = 1, stringsAsFactors = FALSE, header = TRUE)
-  
-  # read the data
-  df <- read.csv(fileName, skip = 2)
-  
-  # assign variable names and labels
-  names(df) <- varNames
-  Hmisc::label(df) <- varLabels 
-  
-  return(df)
-}
+
 
 
 # Correlation matrix with p-values. -----------------------------------------------
